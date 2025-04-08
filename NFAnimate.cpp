9616d7a1b9db6f877372de2ea7dd84fa6ab5c916
trees <kongd@carleton.edu>
2025-04-07 19:25:19 -0500

    did the lab

--- NFAnimate.cpp
+++ NFAnimate.cpp
@@ -1,10 +1,47 @@
 #include "emp/web/Animate.hpp"
 #include "emp/web/web.hpp"
+#include "emp/math/math.hpp"
 
 emp::web::Document doc{"target"};
 
-class NFAnimator : public emp::web::Animate {
+const int width = 50;
+const int height = 50;
+const int cubewidth = 10;
+const int cubeheight = 10;
 
+
+
+class NFAnimator : public emp::web::Animate {
+    emp::web::Canvas canvas{width*cubewidth, height*cubeheight, "canvas"};
+    public:
+        void FindNeighbors(int x, int y) {
+            std::vector<std::vector<int>> neighbors = {
+                {x-1, y-1},
+                {x, y-1},
+                {x+1, y-1},
+                {x-1, y},
+                {x+1, y},
+                {x-1, y+1},
+                {x, y+1},
+                {x+1, y+1},        
+            };
+            canvas.Rect(10*x, 10*y, cubewidth, cubeheight, "black", "black");
+            for (std::vector<int> neighbor : neighbors) {
+                canvas.Rect(10*emp::Mod(neighbor[0], width), 10*emp::Mod(neighbor[1], height), cubewidth, cubeheight, "red", "black");
+            }
+        }
+        NFAnimator() {
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    canvas.Rect(cubewidth*x, cubeheight*y, cubewidth, cubeheight, "white", "black");
+                }
+            }
+            FindNeighbors(0, 0);
+            FindNeighbors(25, 25);
+            doc << canvas;
+            doc << "Hi";
+        }
+        
 };
 
 NFAnimator animator;
